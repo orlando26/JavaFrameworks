14b26ff9be9944b1796e8ba7c3391d7a0ef4e721
Orlando Loza <orlando@LAPTOP-5062TB4C.localdomain>
2020-01-20 20:03:08 -0600

    first commit

---
+++ restServices/src/test/java/com/orlando/restServices/RestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orlando.restServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
