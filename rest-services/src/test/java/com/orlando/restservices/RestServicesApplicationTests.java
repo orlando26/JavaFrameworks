ced2ec1c9cbc64fe6bb135ff55633ad36472163d
Orlando Loza Castro <orlandolozaarr@gmail.com>
2020-03-28 10:52:46 -0600

    spring boot project

---
+++ rest-services/src/test/java/com/orlando/restservices/RestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orlando.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
