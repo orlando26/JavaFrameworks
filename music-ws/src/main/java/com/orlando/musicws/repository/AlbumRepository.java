b790cd86ef1dd86a9d0a89ad4fa1dcea88e05724
Orlando Loza Castro <orlandolozaarr@gmail.com>
2020-05-04 18:55:31 -0500

    may

--- music-ws/src/main/java/com/orlando/musicws/repository/AlbumRepository.java
+++
@@ -1,9 +0,0 @@
-package com.orlando.musicws.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.orlando.musicws.entity.Album;
-
-public interface AlbumRepository extends JpaRepository<Album, Integer>{
-
-}
--- music-ws/src/main/java/com/orlando/musicws/util/UtilConstants.java
+++
@@ -1,9 +0,0 @@
-package com.orlando.musicws.util;
-
-public class UtilConstants {
-	
-	public static final String SUCCESS_MSG = "SUCCESS";
-
-	public static final String ERROR_MSG = "ERROR";
-	
-}
